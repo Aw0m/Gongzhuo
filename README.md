56474885bd84aeba48f9adaab7fa1b7b2a1294ad
liawom@foxmail.com <60684451+Aw0m@users.noreply.github.com>
2022-05-19 11:25:30 +0800

    将数据库的信息读取改变为了读取yaml配置文件的形式

---
+++ README.md
@@ -0,0 +1,12 @@
+# wxPeojectDev
+
+## 1. 简介
+
+微信小程序“共琢”后端部分，实现了一个微服务后端。网关代码见[Aw0m/WxProjectApiGateway](https://github.com/Aw0m/wxprojectApiGateway)
+
+## 2. 部分配置
+
+`user/config/config.yaml`下配置`MySQL`和`Redis`的数据库信息
+
+## 3. 其他
+目前整体仍在开发中
--- user/DAO.go
+++ user/DAO.go
@@ -6,24 +6,56 @@ import (
 	"fmt"
 	"github.com/go-redis/redis/v8"
 	"github.com/go-sql-driver/mysql"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"log"
 )
 
+type Config struct {
+	*MySQLConf `yaml:"MySQL"`
+	*RedisConf `yaml:"Redis"`
+}
+
+type MySQLConf struct {
+	User   string `yaml:"User"`
+	Passwd string `yaml:"Passwd"`
+	Net    string `yaml:"Net"`
+	Addr   string `yaml:"Addr"`
+	DBName string `yaml:"DBName"`
+}
+
+type RedisConf struct {
+	Addr     string `yaml:"Addr"`
+	Password string `yaml:"Password"`
+	DB       int    `yaml:"DB"`
+}
+
+var config *Config
 var db *sql.DB
 var rdb *redis.Client
 
 func init() {
+	yamlFile, err := ioutil.ReadFile("user/config/config.yaml")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic(err)
+	}
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		fmt.Println(err.Error())
+		panic(err)
+	}
 	initMySQL()
 	initRedis()
 }
 
 func initMySQL() {
 	cfg := mysql.Config{
-		User:   "root",
-		Passwd: "lx2001812xx",
-		Net:    "tcp",
-		Addr:   "127.0.0.1:3306",
-		DBName: "wxproject_dev",
+		User:   config.MySQLConf.User,
+		Passwd: config.MySQLConf.Passwd,
+		Net:    config.MySQLConf.Net,
+		Addr:   config.MySQLConf.Addr,
+		DBName: config.MySQLConf.DBName,
 	}
 	var err error
 	db, err = sql.Open("mysql", cfg.FormatDSN())
@@ -39,9 +71,9 @@ func initMySQL() {
 
 func initRedis() {
 	rdb = redis.NewClient(&redis.Options{
-		Addr:     "175.24.163.131:6380",
-		Password: "123456", // no password set
-		DB:       0,        // use default DB
+		Addr:     config.RedisConf.Addr,
+		Password: config.RedisConf.Password, // no password set
+		DB:       config.RedisConf.DB,       // use default DB
 	})
 	ctx := context.Background()
 	val, err := rdb.Get(ctx, "key").Result()
